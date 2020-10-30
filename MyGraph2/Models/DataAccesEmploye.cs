d1006cef764c11f6b6c9e0ede362a9db8c040490
Monzón <58608455+Diaser@users.noreply.github.com>
2020-10-29 21:26:09 -0600

    Agregar archivos de proyecto.

---
+++ MyGraph2/Models/DataAccesEmploye.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGraph2.Models
+{
+    public class DataAccesEmploye
+    {
+        public static List<EmployeModel> getEmployeList()
+        {
+            var list = new List<EmployeModel>();
+            list.Add(new EmployeModel { name = "Pierre lang", manager = "", toolTip = "Board of Directors", workPosition = "Board of Directors" });
+            list.Add(new EmployeModel { name = "Mary Jackson", manager = "Pierre lang", toolTip = "Excecutive Director", workPosition = "Excecutive Director" });
+            list.Add(new EmployeModel { name = "Robert Thompson", manager = "Mary Jackson", toolTip = "Volunters", workPosition = "Volunters" });
+            list.Add(new EmployeModel { name = "David Ellison", manager = "Mary Jackson", toolTip = "Excecutive Staff Manager", workPosition = "Excecutive Staff Manager" });
+            list.Add(new EmployeModel { name = "Carl Stevens", manager = "Robert Thompson", toolTip = "Finance Director", workPosition = "Finance Director" });
+            list.Add(new EmployeModel { name = "Nancy Sakamoto", manager = "Robert Thompson", toolTip = "Fundraising Director", workPosition = "Fundraising Director" });
+            list.Add(new EmployeModel { name = "Tiffin Kiling", manager = "Robert Thompson", toolTip = "Fundraising Director", workPosition = "Operations Director" });
+            list.Add(new EmployeModel { name = "Francis Whitacre", manager = "Robert Thompson", toolTip = "Program Director", workPosition = "Program Director" });
+            list.Add(new EmployeModel { name = "Bety Carison", manager = "Robert Thompson", toolTip = "Communications Director", workPosition = "Communications Director" });
+
+            return list;
+
+        }
+
+    }
+}
---
+++ MyGraph2/Models/EmployeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGraph2.Models
+{
+    public class EmployeModel
+    {
+        public string name { get; set; }
+        public string manager { get; set; }
+        public string workPosition { get; set; }
+        public string toolTip { get; set; }
+    }
+}
